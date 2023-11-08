cc91248b5d6c89ae54d2943c7951b2d9a61d0e26
Gerson G <garciagers@gmail.com>
2023-11-08 14:01:49 -0600

    File name changes

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+// import logo from "./images/logo.svg";
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;

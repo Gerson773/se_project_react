fc816a8339324a2dc9dcc17aa754aee807aec256
Gerson G <garciagers@gmail.com>
2024-04-01 00:42:39 -0500

    corrected basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/se_project_react">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </React.StrictMode>
